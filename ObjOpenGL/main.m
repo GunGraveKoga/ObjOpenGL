f1442d6baf84013996a9fb14fc7bc65ae8a11454
GunGraveKoga <vovk.yury@me.com>
2017-12-14 01:32:47 +0300

    Initial Commit

---
+++ ObjOpenGL/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  ObjOpenGL
+//
+//  Created by Юрий Вовк on 14.12.2017.
+//  Copyright © 2017 Yury Vovk. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
